10e5ae8d2c4a0336b80b8ea5766a973258250d39
MirrorCartographer <mirrorcartographer@gmail.com>
2026-04-26 03:49:36 -0400

    Load fail-safe styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './assets/styles.css';
+import './assets/failsafe.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
