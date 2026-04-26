2d6c76f925706c1c79ee41870f5b2d977ddea7f9
MirrorCartographer <mirrorcartographer@gmail.com>
2026-04-26 04:26:30 -0400

    Export live feed using app-compatible names

--- src/data/liveFeed.js
+++ src/data/liveFeed.js
@@ -1,21 +1,25 @@
 import { episodeBacklog } from './episodeBacklog';
-import { mutationWords, seedCapsules, transitions } from './creationFeed';
+import { mutationWords as baseMutationWords, seedCapsules as baseSeedCapsules, transitions as baseTransitions } from './creationFeed';
 
-export const liveCapsules = [...episodeBacklog, ...seedCapsules];
+export const seedCapsules = [...episodeBacklog, ...baseSeedCapsules];
 
-export const liveTransitions = {
+export const transitions = {
   'episode-003-ocean-static-radio': ['shoreline-synth-aquarium', 'bad-girl-orchestra-lab', 'mural-that-remixes-you'],
   'shoreline-synth-aquarium': ['ocean-terminal', 'episode-003-ocean-static-radio', 'room-that-sings-back'],
   'bad-girl-orchestra-lab': ['chorus-engine-room', 'static-carnival', 'ash-pop-cathedral'],
   'mural-that-remixes-you': ['animated-afterimage-theater', 'afterimage-orchard', 'mirrorstorm-drive-in'],
-  ...transitions
+  ...baseTransitions
 };
 
-export const liveMutationWords = [
-  ...mutationWords,
+export const mutationWords = [
+  ...baseMutationWords,
   'radio',
   'tide',
   'orchestra',
   'mural',
   'signal'
 ];
+
+export const liveCapsules = seedCapsules;
+export const liveTransitions = transitions;
+export const liveMutationWords = mutationWords;
