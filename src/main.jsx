1c619850bb756fb079c6b3ce9c1f1d97e9a64558
MirrorCartographer <mirrorcartographer@gmail.com>
2026-07-16 14:06:48 -0400

    Route composition experience to spatial sound field

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CompositionWorld from './components/CompositionWorld';
+import SpatialSoundField from './components/SpatialSoundField';
 import ErrorBoundary from './components/ErrorBoundary';
 import { installCometGardenRuntime } from './engine/cometGardenRuntime';
 import { installAudioObservabilityRuntime } from './engine/audioObservabilityRuntime';
@@ -26,7 +26,7 @@ installCometGardenRuntime();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <CompositionWorld />
+      <SpatialSoundField />
     </ErrorBoundary>
   </React.StrictMode>,
 );
