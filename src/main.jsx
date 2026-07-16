1402bb2968ae94367534fa4923eb5b9da26fa1ae
MirrorCartographer <mirrorcartographer@gmail.com>
2026-07-16 15:09:23 -0400

    Wire generative score garden as feature entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CompositionWorld from './components/CompositionWorld';
+import GenerativeScoreGarden from './components/GenerativeScoreGarden';
 import ErrorBoundary from './components/ErrorBoundary';
-import { installCometGardenRuntime } from './engine/cometGardenRuntime';
-import { installAudioObservabilityRuntime } from './engine/audioObservabilityRuntime';
-import { installAudioRoutingEvidenceRuntime } from './engine/audioRoutingEvidenceRuntime';
-import { installAudibilityOutcomeRuntime } from './engine/audibilityOutcomeRuntime';
-import { installAudioEvidenceExportRuntime } from './engine/audioEvidenceExportRuntime';
-import { installAudioDeviceEvidenceRuntime } from './engine/audioDeviceEvidenceRuntime';
 import { installDeploymentIdentityRuntime } from './engine/deploymentIdentityRuntime';
 import { installRuntimeInfrastructure } from './services/runtimeBootstrap';
-import './assets/styles.css';
 import './assets/failsafe.css';
-import './assets/comet-garden.css';
 
 installRuntimeInfrastructure();
 installDeploymentIdentityRuntime();
-installAudioDeviceEvidenceRuntime({ window, navigator, document });
-installAudioObservabilityRuntime();
-installAudioRoutingEvidenceRuntime();
-installAudibilityOutcomeRuntime();
-installAudioEvidenceExportRuntime();
-installCometGardenRuntime();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <CompositionWorld />
+      <GenerativeScoreGarden />
     </ErrorBoundary>
   </React.StrictMode>,
 );
